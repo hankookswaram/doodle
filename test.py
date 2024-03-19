0f3dff036e5dbca2c8b26cdb9701b30c319f16ec
hankookswaram <98754301+hankookswaram@users.noreply.github.com>
2024-03-19 11:11:46 +0000

    commit

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-N = int(input())
-buildings = {i: list(map(int, input().split())) for i in range(1, N+1)}
-memo = {}
+from collections import defaultdict
 
+def dfs(n):
+    if A[n] != 0:
+        return A[n]
+    A[n] = dfs(n//P) + dfs(n//Q)
+    return A[n]
+
+N, P, Q = map(int, input().split())
+A = defaultdict(int)
+A[0] = 1
+print(dfs(N))
