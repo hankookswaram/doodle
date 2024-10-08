240562f2de0ff5f6696c9a59ace7e9cfc245d8a0
hankookswaram <98754301+hankookswaram@users.noreply.github.com>
2024-10-08 11:35:55 +0000

    commit

--- stuff.cpp
+++ stuff.cpp
@@ -4,7 +4,18 @@
 using namespace std;
 
 int main() {
-    cout << "Hi";
+    int dp[5001];
+    int input;
+    cin >> input;
+
+    dp[1] = dp[2] = dp[4] = 9999;
+    dp[3] = dp[5] = 1;
+
+    for (int i=6; i<=input; i++) {
+        dp[i] = min(dp[i-3], dp[i-5]) + 1;
+    }
+
+    cout << ((dp[input] == 0 || dp[input] >= 9999) ? -1 : dp[input]);
 
     return 0;
 }
