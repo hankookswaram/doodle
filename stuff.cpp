80b9cc3f677ef5e6737970d37b932b86c42dced4
hankookswaram <98754301+hankookswaram@users.noreply.github.com>
2024-11-05 14:17:43 +0000

    commit

--- stuff.cpp
+++ stuff.cpp
@@ -9,38 +9,12 @@
 #include <cstdlib>
 #include <unordered_map>
 #include <cctype>
+#include <limits>
 
 using namespace std;
 
-vector<vector <int>> graph;
-vector<bool> visited;
-int inf_count = 0;
-
-void dfs(int n) {
-    visited[n] = true;
-    for (int i : graph[n]) {
-        if (!visited[i]) {
-            inf_count++;
-            dfs(i);
-        }
-    }
-}
-
 int main() {
-    int n, m;
-    cin >> n >> m;
 
-    graph.resize(n+1);
-    visited.resize(n+1, false);
-
-    for (int i=0; i<m; i++) {
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
-    dfs(1);
-    cout << inf_count << '\n';
 
     return 0;
 }
